7e6014d874a2663accf47915b9df08a02c1e61e9
Sebastiaan Hols <shg.hols@gmail.com>
2019-01-25 13:57:23 +0100

    Opdracht 2 - ArrayList van Rekeningen gemaakt

--- opdracht_2/src/Bank.java
+++ opdracht_2/src/Bank.java
@@ -2,35 +2,21 @@ import java.util.ArrayList;
 
 public class Bank {
 
-    public static void main(String[] args) {
+    private ArrayList<Rekening> rekeningen;
 
-        System.out.println("\nWelkom bij Hols Bank onze klanten zijn:\n");
-        new Bank();
-
+    public Bank(ArrayList<Rekening> rekeningen) {
+        this.rekeningen = rekeningen;
     }
 
-    ArrayList<Rekening> rekeningen;
-
-    public Bank() {
-        this.rekeningen = new ArrayList();
-        rekeningen.add(new Rekening(8563,"Fabian", 14.56));
-        rekeningen.add(new Rekening(7823,"Fenia", 24.63));
-        rekeningen.add(new Rekening(9356,"Hugo", 5.67));
-
-        System.out.println(getRekening(8563).getNaam());
-    }
-
-    private Rekening getRekening (int rekeningNr) {
+    public Rekening getRekening (int rekeningNr) {
 
         Rekening rekening = null;
 
         for (Rekening i : rekeningen){
-            System.out.println(i.getNaam() + " heeft " + i.getSaldo() + " pingels");
             if (rekeningNr == i.getRekeningNr()){
                 rekening = i;
             }
         }
-
         return rekening;
     }
 
