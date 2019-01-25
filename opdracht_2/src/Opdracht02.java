993dc2be5d72294c1180aa0d381e95469ea5e424
Sebastiaan Hols <shg.hols@gmail.com>
2019-01-25 21:41:23 +0100

    Opdracht 2 - Tests toegevoegd voor Bank

--- opdracht_2/src/Opdracht02.java
+++ opdracht_2/src/Opdracht02.java
@@ -4,6 +4,7 @@ public class Opdracht02 {
 
     public static void main(String[] args) {
 
+// Dataset
         ArrayList<Rekening> rekeningen = new ArrayList();
         rekeningen.add(new Rekening(1111,"Fabian", 14.56));
         rekeningen.add(new Rekening(1234,"Fenia", 24.63));
@@ -11,8 +12,10 @@ public class Opdracht02 {
         rekeningen.add(new Rekening(2345,"Sebas", 5.67));
         rekeningen.add(new Rekening(3333,"Beatrix", 10209.67));
 
+// Create Bank
         Bank bank = new Bank(rekeningen);
 
+// Run tests
         new BankTest(bank);
         new RekeningTest(rekeningen.get(0));
 
