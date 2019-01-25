aab2c60d7ee69756c39ac265059d3e8015f439bc
Sebastiaan Hols <shg.hols@gmail.com>
2019-01-25 11:12:50 +0100

    Added - getRekening

--- opdracht_2/src/Bank.java
+++ opdracht_2/src/Bank.java
@@ -5,16 +5,33 @@ public class Bank {
     public static void main(String[] args) {
 
         System.out.println("\nWelkom bij Hols Bank onze klanten zijn:\n");
+        new Bank();
 
-        ArrayList<Rekening> rekeningen = new ArrayList();
+    }
+
+    ArrayList<Rekening> rekeningen;
+
+    public Bank() {
+        this.rekeningen = new ArrayList();
         rekeningen.add(new Rekening(8563,"Fabian", 14.56));
         rekeningen.add(new Rekening(7823,"Fenia", 24.63));
         rekeningen.add(new Rekening(9356,"Hugo", 5.67));
 
+        System.out.println(getRekening(8563).getNaam());
+    }
+
+    private Rekening getRekening (int rekeningNr) {
+
+        Rekening rekening = null;
+
         for (Rekening i : rekeningen){
-            System.out.println(i.getNaam());
+            System.out.println(i.getNaam() + " heeft " + i.getSaldo() + " pingels");
+            if (rekeningNr == i.getRekeningNr()){
+                rekening = i;
+            }
         }
 
+        return rekening;
     }
 
 }
--- opdracht_2/src/Rekening.java
+++ opdracht_2/src/Rekening.java
@@ -10,7 +10,21 @@ public class Rekening {
         this.saldo = saldo;
     }
 
-    public String getNaam (){
+    protected String getNaam (){
         return this.naam;
     }
+    protected double getSaldo () { return this.saldo; }
+    protected int getRekeningNr () { return this.rekeningNr; }
+
+
+    protected void setSaldo (double bedrag) {
+        if (saldo + bedrag > 0 ){
+            saldo += bedrag;
+            System.out.println("Overschrijven is gelukt het nieuwe saldo is: " + saldo);
+        } else {
+            System.out.println("Overschrijven is NIET gelukt: onvoldoende saldo");
+        }
+    }
+
+
 }
