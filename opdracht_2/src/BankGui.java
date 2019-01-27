a060792331fec6a97559c5ca7474cc108c3b41bc
Sebastiaan Hols <shg.hols@gmail.com>
2019-01-27 17:46:27 +0100

    Opdracht 2 - Addning GUI

--- opdracht_2/src/BankGui.java
+++ opdracht_2/src/BankGui.java
@@ -7,6 +7,15 @@ public class BankGui {
     private JPanel creditRekening;
     private JTextField debitRekeningNummerInput;
     private JTextField debitBedragInput;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField creditRekeningNummerImput;
+    private JTextField creditBedragInput;
+    private JLabel Rekeningnummer;
+    private JLabel Bedrag;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
 }
