f6a7719bb1205d68a48d7d965e994e358f657109
Sebastiaan Hols <shg.hols@gmail.com>
2019-02-03 12:37:29 +0100

    Specify tests

--- opdracht_2/src/tests/Opdracht02Test.java
+++ opdracht_2/src/tests/Opdracht02Test.java
@@ -10,19 +10,16 @@ public class Opdracht02Test {
     public static void main(String[] args) {
         // Dataset
         ArrayList<Rekening> rekeningen = new ArrayList();
-        rekeningen.add(new Rekening(1111, "Fabian", 14.56));
-        rekeningen.add(new Rekening(1234, "Fenia", 24.63));
-        rekeningen.add(new Rekening(2222, "Hugo", 15.67));
         rekeningen.add(new Rekening(2345, "Sebas", 5.67));
         rekeningen.add(new Rekening(3333, "Beatrix", 10209.67));
 
 // Actieve rekeningen
-        int debitRekeningNr = 1111;
-        int creditRekeningNr = 1234;
+        int debitRekeningNr = 2345;
+        int creditRekeningNr = 3333;
 
 // Create bank.Bank
         Bank bank = new Bank(rekeningen, debitRekeningNr, creditRekeningNr);
-        BankFrame bankFrame = new BankFrame(bank);
+
 // Run tests
         new tests.BankTest(bank);
         new tests.RekeningTest(rekeningen.get(0));
