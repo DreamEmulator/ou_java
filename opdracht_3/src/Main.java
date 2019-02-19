228c231043fbc7e1f6bdcd4db09768adaedc1cfd
Sebastiaan Hols <shg.hols@gmail.com>
2019-02-19 18:34:52 +0100

    Let's get started

---
+++ opdracht_3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * Hierbij de main-method van de Opdracht
+     * @param args wordt wellicht gebruikt om parameters mee te kunnen geven vanaf de commandline
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
