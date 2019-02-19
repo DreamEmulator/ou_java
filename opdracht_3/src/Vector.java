4a32c5400fbbc889c64950a43204e4e8e2e3cf9b
Sebastiaan Hols <shg.hols@gmail.com>
2019-02-19 19:46:34 +0100

    Let's get started

---
+++ opdracht_3/src/Vector.java
@@ -0,0 +1,74 @@
+public class Vector {
+    /**
+     * Hierbij de main-method van de Opdracht.
+     * @param args wordt wellicht gebruikt om parameters mee te kunnen geven vanaf de commandline
+     */
+    static void main(String[] args) {
+        System.out.println("Opdracht 03 is aan!");
+    }
+
+    /**
+     * Stub: om de lengte van deze vector te berekenen.
+     * @return
+     */
+    double getLength (){
+        return 0.00;
+    }
+
+    /**
+     * Stub: om de afstand tussen deze vector en v te bepalen.
+     * @return
+     */
+    double getAfstand(Vector v) {
+        return 0.00;
+    }
+
+    /**
+     * Stub: om de afstand tussen deze vector en v te bepalen.
+     * @return
+     */
+    Vector plus(Vector v){
+        return v;
+    }
+
+    /**
+     * Stub: om te bepalen of deze vector gelijk is aan v.
+     * @return
+     */
+    boolean equals(Vector v){
+        return false;
+    }
+
+    /**
+     * Stub: om een nieuwe vector op te leveren verkregen door de x-waarde en de y-waarde van deze vector te vermenigvuldigen met d.
+     * @return
+     */
+    Vector maal(double d){
+        Vector v = new Vector();
+        return v;
+    }
+
+    /**
+     * Stub: om aan te geven of deze vector en v dezelfde richting hebben.
+     * @return
+     */
+    boolean heeftZelfdeRichting(Vector v){
+        return false;
+    }
+
+    /**
+     * Stub: om het inwendig product van deze vector en v te bepalen.
+     * @return
+     */
+    double getInproduct(Vector v){
+        return 0.00;
+    }
+
+    /**
+     * Stub: om de hoek van deze vector met de x-as te bepalen.
+     * @return
+     */
+    double getHoek(){
+        return 0.00;
+    }
+}
---
+++ opdracht_3/src/VectorTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+public class VectorTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getLength() {
+    }
+
+    @org.junit.Test
+    public void getAfstand() {
+    }
+
+    @org.junit.Test
+    public void plus() {
+    }
+
+    @org.junit.Test
+    public void equals() {
+    }
+
+    @org.junit.Test
+    public void maal() {
+    }
+
+    @org.junit.Test
+    public void heeftZelfdeRichting() {
+    }
+
+    @org.junit.Test
+    public void getInproduct() {
+    }
+
+    @org.junit.Test
+    public void getHoek() {
+    }
+}
