dbb3c451dca2e7a505952dd7d5292cbc8cf7c488
DreamEmulator <shg.hols@gmail.com>
2019-03-31 13:59:59 +0200

    Klaar

--- opdracht_4/src/theater/Theater.java
+++ opdracht_4/src/theater/Theater.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 public class Theater {
     public final static int AANTALTRIJEN = 15;
     public final static int AANTALPERRIJ = 10;
-
     private int hoogsteKlantnummer = 0;
     private String naam;
     private ArrayList<Klant> klanten = new ArrayList<>();
