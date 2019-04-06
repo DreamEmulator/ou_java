655757dfa28ec0059be1a0294e617df08ad6d9af
dreamEmulator <JRK18sab>
2019-04-06 16:16:27 +0200

    Boilerplate

--- advanced_object_oriented_programming/opdracht_01/src/studentenadmin/StudentenAdministratie.java
+++ advanced_object_oriented_programming/opdracht_01/src/studentenadmin/StudentenAdministratie.java
@@ -1,9 +1,11 @@
 package studentenadmin;
 
+
 import studentenadmingui.StudentenAdministratieFrame;
 
 public class StudentenAdministratie {
     public static void main(String[] args) {
         new StudentenAdministratieFrame();
+
     }
 }
