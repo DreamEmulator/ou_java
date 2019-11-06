230a40d4bbfdc8b51e784eb9b830b7389fe2fee0
Sebastiaan Hols <shg.hols@gmail.com>
2019-11-06 20:46:03 +0100

    Trim and clean all synonyms

--- datastructuren_en_algoritmen/2/opdr2/src/opdr2/Thesaurus.java
+++ datastructuren_en_algoritmen/2/opdr2/src/opdr2/Thesaurus.java
@@ -1,4 +1,5 @@
 package opdr2;
+
 import java.util.*;
 
 public class Thesaurus {
@@ -23,36 +24,36 @@ public class Thesaurus {
         woordenlijst.add(woord);
 
         TreeSet<String> synoniemenSet = new TreeSet<>(new compareLength().thenComparing(new compareAlphabet()));
+        for (int s = 0; s < synoniemen.length; s++) {
+            synoniemen[s] = synoniemen[s].toLowerCase().trim();
+        }
         synoniemenSet.addAll(Arrays.asList(synoniemen));
-
-        synoniemenlijst.put(woord,synoniemenSet);
+        synoniemenlijst.put(woord, synoniemenSet);
     }
 
     void checkWoordUniek(String woord) throws ThesaurusException {
-        if (woordenlijst.contains(woord)){
+        if (woordenlijst.contains(woord)) {
             throw new ThesaurusException("Let op: Woord bestaat al in de woordenlijst");
         }
     }
 
     class compareAlphabet implements Comparator<String> {
-        public int compare(String str1, String str2)
-        {
+        public int compare(String str1, String str2) {
             return str1.compareTo(str2);
         }
     }
 
     class compareLength implements Comparator<String> {
-        public int compare(String str1, String str2)
-        {
+        public int compare(String str1, String str2) {
             return str1.length() - str2.length();
         }
     }
 
-    String[] getWoordenlijst(){
+    String[] getWoordenlijst() {
         return woordenlijst.toArray(new String[0]);
     }
 
-    Set<String> getSynoniemenLijst(String woord){
+    Set<String> getSynoniemenLijst(String woord) {
         return synoniemenlijst.get(woord);
     }
 }
